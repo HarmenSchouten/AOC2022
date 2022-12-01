b34929c9d2c505e9cc664bc6536bf1b48922ad9d
Harmen <harmenschouten@outlook.com>
2022-12-01 20:13:30 +0100

    Cleanup part 2

--- Day1/Part2.ts
+++ Day1/Part2.ts
@@ -1,21 +1,8 @@
 const text = await Deno.readTextFile("./Day1/input.txt")
 
-const items = text.split("\r\n")
-
-type CalorieCount = {
-    number: number
-}
-
-const list = items.reduce((acc, item) => {
-    if (item.trim() === "") {
-        acc.push({} as CalorieCount)
-        return acc
-    } else {
-        const last = acc.pop()
-        return [...acc, {number: Number(last?.number ?? 0) + Number(item)}]
-    }
-}, [] as CalorieCount[]).sort((a,b) => a.number - b.number).slice(-3)
-
-
-console.log(list.reduce((acc, item) => acc += item.number, 0))
-
+console.log(text.split("\r\n").reduce((acc, item) => {
+    item.trim() === ""
+        ? acc.push(0)
+        : acc.push((acc.pop() ?? 0 )+ Number(item))
+    return acc
+}, [] as number[]).sort((a,b) => a - b).slice(-3).reduce((acc, item) => acc += item, 0))
