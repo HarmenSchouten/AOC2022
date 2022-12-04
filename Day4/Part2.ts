60c18d6b7bece4bcd26acd6b4a0c0f806ce87075
Harmen <harmenschouten@outlook.com>
2022-12-04 06:20:39 +0100

    Clean up

--- Day4/Part2.ts
+++ Day4/Part2.ts
@@ -6,18 +6,9 @@ console.log(items.reduce((acc, item) => {
     const rangeA = [Number(split[0].split("-")[0]), Number(split[0].split("-")[1])]
     const rangeB = [Number(split[1].split("-")[0]), Number(split[1].split("-")[1])]
 
-    const arrA = [] as number[]
-    for (let i = rangeA[0]; i<= rangeA[1]; i++) {
-        arrA.push(i)
-    }
-
-    const arrB = [] as number[]
-    for (let i = rangeB[0]; i<= rangeB[1]; i++) {
-        arrB.push(i)
-    }
-
-    if (arrA.filter(value => arrB.includes(value)).length >= 1) {
+    if (rangeB[0] <= rangeA[1] && rangeA[0] <= rangeB[1]) {
         return acc += 1
     }
+
     return acc
 }, 0))
