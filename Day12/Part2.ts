e689b39fc80fa124852a4e866981566d9adb38ac
Harmen <harmenschouten@outlook.com>
2022-12-12 20:50:44 +0100

    Reverse search part 2

--- Day12/Part2.ts
+++ Day12/Part2.ts
@@ -10,7 +10,7 @@ type Coord = {
 
 const alphabet = [..."abcdefghijklmnopqrstuvwxyz"]
 
-const findStart = () => {
+const findEnds = () => {
     const startCoords = [] as Coord[]
     for (let i = 0; i < grid.length; i++)
     for (let j = 0; j < grid[i].length; j++)
@@ -20,7 +20,7 @@ const findStart = () => {
     return startCoords
 }
 
-const findEnd = () => {
+const findStart = () => {
     for (let i = 0; i < grid.length; i++)
     for (let j = 0; j < grid[i].length; j++)
         if (grid[i][j] === "E") {
@@ -35,83 +35,84 @@ const neighbours = (x:number, y:number) => [
     {x: x + 1, y: y},
 ]
 
+const ends = findEnds()!
 const start = findStart()!
-const end = findEnd()!
 
-const risks = start.reduce((acc, start) => {
-    let queue: Record<string, number> = {}
-    queue[`${start.x}.${start.y}`] = 0
+let queue: Record<string, number> = {}
+queue[`${start.x}.${start.y}`] = 0
 
-    const riskMap = {} as Record<string, number>
-    riskMap[`${start.x}.${start.y}`] = 0
+const riskMap = {} as Record<string, number>
+riskMap[`${start.x}.${start.y}`] = 0
 
-    const hasItem = (x: number, y:number, grid: string[][]) => {
-        const line = grid[y]
-        const item = line?.[x] ?? undefined
-        return item ?? undefined
-    }
+const hasItem = (x: number, y:number, grid: string[][]) => {
+    const line = grid[y]
+    const item = line?.[x] ?? undefined
+    return item ?? undefined
+}
 
-    const getLowest = (map:Record<string, number>) => {
-        const arr = Object.keys(map);
-        if (arr.length > 0) {
-            const lowestItem = arr.reduce((acc, item) => {
-                if (map[item] < map[acc]) {
-                    return item 
-                } else {
-                    return acc
-                }
-            })
-        
-            delete map[lowestItem]
-            
-            queue = map
-        
-            return lowestItem    
-        } 
-        return undefined
-    
-    }
-
-    while (true) {
-        const pointString = getLowest(queue)
-    
-        if (pointString === undefined) {
-            break;
-        }
-    
-        const point = {
-            x: Number(pointString.split('.')[0]),
-            y: Number(pointString.split('.')[1])
-        } as Coord
-    
-        if (point.x === end.x && point.y === end.y) {
-            break;
-        }
-    
-        const adjacent = neighbours(point.x, point.y)
-        adjacent.forEach(item => {
-            const gridItem = hasItem(item.x, item.y, grid)
-            const stringItem = gridItem === "S"
-                ? "a"
-                : gridItem === 'E'
-                    ? 'z'
-                    : gridItem
-            if (gridItem) {
-                if (alphabet.indexOf(stringItem) - alphabet.indexOf(grid[point.y][point.x]) <= 1) {
-                    const totalRisk = riskMap[`${point.x}.${point.y}`] + 1//HasItem(item.x, item.y, grid)!
-                    if (totalRisk < (riskMap[`${item.x}.${item.y}`] ?? Number.MAX_SAFE_INTEGER)) {
-                        riskMap[`${item.x}.${item.y}`] = totalRisk
-                        queue[`${item.x}.${item.y}`] = totalRisk
-                    }
-                }
+const getLowest = (map:Record<string, number>) => {
+    const arr = Object.keys(map);
+    if (arr.length > 0) {
+        const lowestItem = arr.reduce((acc, item) => {
+            if (map[item] < map[acc]) {
+                return item 
+            } else {
+                return acc
             }
         })
+    
+        delete map[lowestItem]
+        
+        queue = map
+    
+        return lowestItem    
+    } 
+    return undefined
+}
+
+const getString = (value:string) => {
+    if (value === "S") {
+        return "a"
+    }
+    if (value === "E") {
+        return "z"
+    }
+    return value
+}
+
+while (true) {
+    const pointString = getLowest(queue)!
+
+    const point = {
+        x: Number(pointString.split('.')[0]),
+        y: Number(pointString.split('.')[1])
+    } as Coord
+
+    if (ends.some(end => end.x === point.x && end.y === point.y)) {
+        break;
     }
 
-    if (riskMap[`${end.x}.${end.y}`] < acc) {
-        return riskMap[`${end.x}.${end.y}`]!
+    neighbours(point.x, point.y).forEach(item => {
+        const gridItem = hasItem(item.x, item.y, grid)
+        if (gridItem) {
+            if (alphabet.indexOf(getString(grid[point.y][point.x])) - alphabet.indexOf(getString(gridItem)) <= 1) {
+                const totalRisk = riskMap[`${point.x}.${point.y}`] + 1
+                if (totalRisk < (riskMap[`${item.x}.${item.y}`] ?? Number.MAX_SAFE_INTEGER)) {
+                    riskMap[`${item.x}.${item.y}`] = totalRisk
+                    queue[`${item.x}.${item.y}`] = totalRisk
+                }
+            }
+        }
+    })
+}
+
+const minRisk = ends.reduce((acc, end) => {
+    const risk = riskMap[`${end.x}.${end.y}`];
+    if (risk < acc) {
+        return risk
+    } else {
+        return acc
     }
-    return acc
 }, Number.MAX_SAFE_INTEGER)
 
-console.log(risks)
+console.log(minRisk)
