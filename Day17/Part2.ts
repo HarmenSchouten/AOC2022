8473d5c7cacda6ede5bd3573a3ee9023e95b5b1f
Harmen <harmenschouten@outlook.com>
2022-12-17 13:06:21 +0100

    Parte Dois

---
+++ Day17/Part2.ts
@@ -0,0 +1,78 @@
+const text = await Deno.readTextFile("./Day17/input.txt")
+const jets = [...text.split("\r\n")[0]]
+
+const width = 7;
+const margin = 3;
+const startX = 2;
+
+const rocks = [
+    ["####"],                       // MINUS        
+    [".#.", "###",".#."],           // PLUS
+    ["###", "..#", "..#"],          // L
+    ["#","#","#","#"],              // I
+    ["##", "##"],                   // SQUARE
+]
+
+let jetIndex = 0;
+
+const checkOverlap = (chamber: string[][], rock:string[], x:number, y:number) => {
+    return (x < 0) 
+    || (x + rock[0].length > 7)
+    || (y < 0)
+    || rock.some((row, rowIndex) => (rowIndex + y) < chamber.length && [...row].some((cell, cellIndex) => cell === "#" && chamber[y + rowIndex][cellIndex + x] === "#"))
+}
+
+const dropRock = (chamber:string[][], rock:string[], x:number, y:number) => {
+
+    while (!checkOverlap(chamber, rock, x, y)) {
+        // Get the current jetblast
+        const jetBlast = jets[jetIndex % jets.length]
+                
+        x += jetBlast === ">" ? 1 : -1
+
+        if(checkOverlap(chamber, rock, x, y)) x += jetBlast === ">" ? -1 : 1
+
+        // Increase the jetIndex
+        jetIndex++
+
+        y--
+    }
+
+    y++
+
+    // Fill el chamber
+    while (rock.length + y > chamber.length) {
+        chamber.push(new Array(width).fill("."))
+    }
+
+    rock.forEach((row, rowIndex) => {
+        [...row].forEach((cell, cellIndex) => {
+            if (cell === "#") {
+                chamber[y + rowIndex][x + cellIndex] = "#"
+            }
+        })
+    })
+
+    return chamber
+}
+
+let chamber = [] as string[][]
+const cycle = new Set<number>()
+const cycleMap:Record<number, number> = {}
+for (let i = 0; i < 5000 && cycle.size < 2; i++) {
+    const rock = rocks[i % 5];
+    const x = startX;
+    const y = chamber.length + margin
+    chamber = dropRock(chamber, rock, x, y)
+    cycleMap[i] = chamber.length
+    if (chamber[chamber.length - 1].every(cell => cell === "#")) {
+        cycle.add(i)
+    }
+}
+
+const diff = [...cycle][1] - [...cycle][0]
+const startCycle = cycleMap[[...cycle][0]]
+const blockDiff = cycleMap[[...cycle][1]] - cycleMap[[...cycle][0]]
+
+const division = ((1000000000000 - [...cycle][0]) / diff)
+console.log(Math.round(division * blockDiff + startCycle))
