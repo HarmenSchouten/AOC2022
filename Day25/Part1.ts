c5ed7a362e56411126728074b69c5e0d63ea217b
Harmen <harmenschouten@outlook.com>
2022-12-25 10:30:13 +0100

    Day 25 P1

--- Day25/Part1.ts
+++ Day25/Part1.ts
@@ -1,2 +1,28 @@
 const text = await Deno.readTextFile("./Day25/input.txt")
 const items = text.split("\r\n")
+
+const chars = [ '=', '-', '0', '1', '2' ]
+const [ base, offset ] = [ 5, 2 ];
+
+/** converts SNAFU to decimal */
+function toDecimal( snafu: string ): number {
+    return snafu.split( '' ).reverse().reduce(
+        (total, symbol, i) => total + base**i * (chars.indexOf(symbol) - offset),
+        0
+    );
+}
+
+/** converts decimal to SNAFU */
+function toSnafu( num: number ): string {
+    const digits = [];
+    while ( num > 0 ) {
+        // SNAFU is base 5, but each digits place is shifted by 2
+        num += offset;
+        digits.unshift( num % base );
+        num = Math.floor( num / base );
+    }
+    // convert regular digits to SNAFU glyphs
+    return digits.map( digit => chars[digit] ).join( '' );
+}
+
+console.log('Answer:', toSnafu(items.map(toDecimal).reduce((acc, item) => acc += item, 0)));
